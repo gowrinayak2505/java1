10669cad1918a7890922b72eba87e5c71f48aa42
gowrinayak2505 <nayakgowri79@gmail.com>
2024-09-24 16:06:05 +0530

    Human,Teacher,Doctor

---
+++ G4.java
@@ -0,0 +1,20 @@
+ class Human{
+     public void job(){
+	  System.out.println("Working professional");}}
+  class Teacher extends Human{
+      public void job(){
+	  System.out.println("Teacher");}}
+class Doctor extends Human{
+    public void job(){
+         System.out.println("Doctor");}}
+ public class G4{
+	public static void main(String[] args){
+          Human amar=new Human();
+          Teacher babu=new Teacher();
+	  Doctor chandra=new Doctor();
+ 	  Human dinesh=new Doctor();
+	  amar.job();
+          babu.job();
+          chandra.job();
+          dinesh.job();}}
+         
